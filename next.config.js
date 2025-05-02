45e0b6e98aa6790700d9c3ce1883fafe6b5d0228
gunadharP-aat <gunadhar.patil@aimedatsolutions.com>
2025-05-02 14:22:51 +0530

    feat: added first build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-module.exports = {
-    images: {
-      formats: ['image/avif', 'image/webp',],
-    },
-  };
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    unoptimized: true, // Required for static export
+  },
+};
+
+module.exports = nextConfig;
